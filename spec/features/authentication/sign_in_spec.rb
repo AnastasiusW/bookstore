45060e11c01c11b9d658a0a5125bb7a2951be44c
AnastasiusW <repositoryanastasiia@gmail.com>
2020-03-16 00:22:23 +0200

    Feature test add

---
+++ spec/features/authentication/sign_in_spec.rb
@@ -0,0 +1,46 @@
+RSpec.describe 'Sign in', type: :feature do
+    let!(:user) { create(:user) }
+    let(:sign_in_page) { SignInPrism.new }
+    let(:home_page) { HomePage.new }
+
+    before do
+        sign_in_page.load
+    end
+
+    context 'when try sign in with valid data' do
+        let(:sucessful_sign_in) { I18n.t('devise.sessions.signed_in') }
+        it 'when try sign in exist user' do
+        sign_in_page.sign_in(user.email, user.password)
+        expect(sign_in_page).to have_current_path(root_path)
+        expect(home_page).to have_content(user.email)
+        expect(home_page.flash_success_message).to have_content(I18n.t('devise.sessions.signed_in'))
+    end
+end
+
+
+    context 'with invalid data' do
+        let(:invalid_email) {'wrong@gmail.com'}
+        let(:invalid_password) { 'wrong_password'}
+
+        it 'shows an error message when invalid email' do
+            sign_in_page.sign_in(invalid_email, user.password)
+            expect(sign_in_page.flash_fail_message).to have_content(I18n.t('test_error.invalid_sign_in'))
+            expect(sign_in_page).to have_current_path(new_user_session_path)
+        end
+
+
+        it 'shows an error message when invalid password' do
+            sign_in_page.sign_in(user.email, invalid_password)
+            expect(sign_in_page.flash_fail_message).to have_content(I18n.t('test_error.invalid_sign_in'))
+            expect(sign_in_page).to have_current_path(new_user_session_path)
+        end
+    end
+    context 'when user forgot password' do
+            it 'when click to forgot password link' do
+                click_link(I18n.t('authentication.forgot_password'))
+                expect(sign_in_page).to have_current_path(new_user_password_path)
+            end
+        end
+end
+
+
---
+++ spec/features/support/sign_in.rb
@@ -0,0 +1,17 @@
+class SignInPrism < SitePrism::Page
+    set_url '/users/sign_in'
+    element :name_email, "input[name='user[email]']"
+    element :name_password, "input[name='user[password]']"
+    element :flash_fail, '#flash_fail_id'
+
+    def sign_in(email, password)
+        name_email.set(email)
+        name_password.set(password)
+        click_button(I18n.t('authentication.login'))
+    end
+
+    def flash_fail_message
+        flash_fail.text
+    end
+
+end
