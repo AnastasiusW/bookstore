ff8b618ba43bd39cf7ac51cc4111c5b7d0ab76ed
AnastasiusW <repositoryanastasiia@gmail.com>
2020-03-25 23:36:50 +0200

    Remove to i18n devise text

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,5 @@
 RSpec.describe User, type: :model do
   context 'with check validates' do
-    it 'when valid  email' do
-      expect(build(:user, email: FFaker::Internet.email)).to be_valid
-    end
-
-    it 'when invalid  email' do
-      expect(build(:user, email: nil)).not_to be_valid
-    end
+    it { is_expected.to validate_presence_of(:email) }
   end
 end
