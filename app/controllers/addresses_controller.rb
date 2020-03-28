db538c5f16d557099ed1247a765c8a488bd34b3a
AnastasiusW <repositoryanastasiia@gmail.com>
2020-03-28 17:09:15 +0200

    Repaire logig post and patch

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,30 +1,35 @@
 class AddressesController < ApplicationController
 
     def create
-
         address_form = AddressForm.new(address_params)
-        binding.pry
+
         if address_form.save(current_user)
             flash[:notice] = t('notification.success.address.update', type: address_params[:type])
         else
             flash[:alert] = address_form.errors.full_messages.to_sentence
         end
+        redirect_to edit_user_path(current_user)
     end
 
 
     def update
 
         address_form = AddressForm.new(address_params)
-        binding.pry
+
         if address_form.save(current_user)
             flash[:notice] = t('notification.success.address.update', type: address_params[:type])
         else
             flash[:alert] = address_form.errors.full_messages.to_sentence
         end
+        redirect_to edit_user_path(current_user)
     end
 
 
     def address_params
-        params.require(:billing_address || :shipping_address).permit(:first_name, :last_name, :country, :city, :address, :zip, :phone, :type)
+        if params[:billing_address]
+            params.require(:billing_address).permit(:first_name, :last_name, :country, :city, :address, :zip, :phone, :type)
+        else
+            params.require(:shipping_address).permit(:first_name, :last_name, :country, :city, :address, :zip, :phone, :type)
+        end
     end
 end
