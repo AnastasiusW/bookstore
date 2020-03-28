7631cd7b7e8760aa65e754f0c9921fbf495ddb34
AnastasiusW <repositoryanastasiia@gmail.com>
2020-03-28 14:33:15 +0200

    Add some logic Before method change

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,21 +1,25 @@
 class AddressesController < ApplicationController
 
     def create
+
         address_form = AddressForm.new(address_params)
+        binding.pry
         if address_form.save(current_user)
             flash[:notice] = t('notification.success.address.update', type: address_params[:type])
         else
-            flash[:alert] = t('notification.fail.address.update', type: address_params[:type])
+            flash[:alert] = address_form.errors.full_messages.to_sentence
         end
     end
 
 
     def update
+
         address_form = AddressForm.new(address_params)
+        binding.pry
         if address_form.save(current_user)
             flash[:notice] = t('notification.success.address.update', type: address_params[:type])
         else
-            flash[:alert] = t('notification.fail.address.update', type: address_params[:type])
+            flash[:alert] = address_form.errors.full_messages.to_sentence
         end
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,24 @@ class UsersController < ApplicationController
     def edit
         @presenter = Presenters::Address.new(current_user: current_user)
     end
+
+
+    def update
+        if  user_params[:current_password].present? ? current_user.update_with_password(user_params) : current_user.update(user_params)
+          redirect_to edit_user_path, notice: I18n.t('notification.success.settings.update')
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        current_user.destroy
+        redirect_to root_path, notice: I18n.t('devise.registrations.destroyed')
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :current_password, :password, :password_confirmation)
+      end
 end
