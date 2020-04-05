222b58f285bc497965f2dc623150fb8aae3fd00e
AnastasiusW <repositoryanastasiia@gmail.com>
2020-04-05 16:17:42 +0300

    Fix some spec because it need in admin panel

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -3,10 +3,65 @@ ActiveAdmin.register Book do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 
-   permit_params :title, :description, :price, :quantity,
+  permit_params :title, :description, :price, :quantity,
                  :width, :height, :depth, :year, :material, :category_id, author_ids: []
 
+  decorate_with BookDecorator
 
+
+  index do
+    selectable_column
+    id_column
+    column :category
+    column :title
+    column :authors do |book|
+       book.authors_names
+    end
+    column :description do |book|
+      truncate(book.description, length: Presenters::Show::DESCRIPTION_LIMIT)
+    end
+    column :price do |book|
+      number_to_currency(book.price, unit: '€')
+    end
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :description
+      row :category
+      row :authors do |book|
+        book.authors_names
+     end
+      row :price
+      row :quantity
+      row :width
+      row :height
+      row :depth
+      row :year
+      row :material
+      row :created_at
+      row :updated_at
+    end
+  end
+
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :category
+      f.input :authors, as: :check_boxes, collection: AuthorDecorator.decorate_collection(Author.all)
+      f.input :description
+      f.input :year
+      f.input :price
+      f.input :material
+      f.input :height
+      f.input :width
+      f.input :depth
+    end
+    f.actions
+  end
 
 
 end
--- spec/decorators/book_decorator_spec.rb
+++ spec/decorators/book_decorator_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe BookDecorator do
     ]
   end
 
-  let(:book_test) { create(:book, height: 6.4, width: 0.9, depth: 5.0, authors: authors) }
+  let(:book_test) { create(:book, height: 6.4, width: 1.1, depth: 5.0, authors: authors) }
 
   it '#authors_names' do
     expect(decorator.authors_names).to eq('Fenimore Cooper, Victor  Hugo')
   end
 
   it '#dimensions' do
-    expect(decorator.dimensions).to eq('H: 6.4 " x W: 0.9" x D: 5.0"')
+    expect(decorator.dimensions).to eq('H: 6.4 " x W: 1.1" x D: 5.0"')
   end
 end
