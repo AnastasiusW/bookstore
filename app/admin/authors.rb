feddcebe4e438c27ec88d2f2621637e3bb148839
AnastasiusW <repositoryanastasiia@gmail.com>
2020-04-06 13:34:27 +0300

    Add categories admin  and depend destroy to Category model on book

--- app/admin/authors.rb
+++ app/admin/authors.rb
@@ -1,7 +1,9 @@
 ActiveAdmin.register Author do
+  #actions :all, except: :destroy
    permit_params :first_name, :last_name, :biography
    remove_filter :authors_books
-   config.remove_action_item(:destroy)
+   #config.remove_action_item(:destroy)
+
 
    index do
     selectable_column
@@ -9,10 +11,15 @@ ActiveAdmin.register Author do
     column :first_name
     column :last_name
     column :biography
-    actions
+    column I18n.t('admin.authors.view') do |author|
+      link_to I18n.t('admin.authors.view'), admin_author_path(author)
+    end
+    column I18n.t('admin.authors.edit') do |author|
+      link_to I18n.t('admin.authors.edit'), edit_admin_author_path(author)
+    end
     column I18n.t('active_admin.delete') do |author|
       link_to I18n.t('active_admin.delete'), admin_author_path(author),
-     method: :delete, data: { confirm: I18n.t('admin.authors.confirmations', quantity: author.books.count) }
+              method: :delete, data: { confirm: t('admin.authors.confirmations', quantity: author.books.count) }
    end
   end
 end
