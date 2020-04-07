f32e2878b1f8f05d2063ffd26b83800c90962214
AnastasiusW <repositoryanastasiia@gmail.com>
2020-04-07 19:33:43 +0300

    Add biography author to factory bot

--- spec/features/support/admin/admin_authors.rb
+++ spec/features/support/admin/admin_authors.rb
@@ -1,16 +1,19 @@
 class AdminAuthorsPrism < SitePrism::Page
     element :author_first_name, "input[name='author[first_name]']"
     element :author_last_name, "input[name='author[last_name]']"
+    element :author_bigraphy, "textarea[name='author[biography]']"
 
     def create_form(author)
         author_first_name.set(author.first_name)
         author_last_name.set(author.last_name)
+        author_bigraphy.set(author.biography)
         click_button('Create Author')
     end
 
-    def edit_form(first_name,last_name)
+    def edit_form(first_name,last_name,biography)
         author_first_name.set(first_name)
         author_last_name.set(last_name)
+        author_bigraphy.set(biography)
         click_button('Update Author')
     end
 end
