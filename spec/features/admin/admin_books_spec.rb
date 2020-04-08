11df69fb41e5b4bacef5795c203e7aee6b95a937
AnastasiusW <repositoryanastasiia@gmail.com>
2020-04-08 13:26:06 +0300

    After fix rubocop1

--- spec/features/admin/admin_books_spec.rb
+++ spec/features/admin/admin_books_spec.rb
@@ -1,21 +1,21 @@
-RSpec.describe 'Admin Books' do
-    let!(:book) {create(:book)}
-    let!(:admin_user){create(:admin_user)}
-    let(:admin_books_page) {AdminBooksPrism.new }
-    let(:book_new) {build(:book)}
-    before do
-      sign_in admin_user
-      visit admin_books_path
-    end
+RSpec.describe 'Admin books' do
+  let!(:book) { create(:book) }
+  let!(:admin_user) { create(:admin_user) }
+  let(:admin_books_page) { AdminBooksPrism.new }
+  let(:book_new) { build(:book) }
 
-    it 'when admin create a book' do
-      expect(Book.count).to eq(1)
-      click_link('New Book')
-      admin_books_page.fill_form(book_new,book)
-      expect(admin_books_page).to have_content(book_new.title)
-      expect(Book.count).to eq(2)
-    end
+  before do
+    sign_in admin_user
+    visit admin_books_path
+  end
 
+  it 'when admin create a book' do
+    expect(Book.count).to eq(1)
+    click_link('New Book')
+    admin_books_page.fill_form(book_new, book)
+    expect(admin_books_page).to have_content(book_new.title)
+    expect(Book.count).to eq(2)
+  end
 
   it 'when admin click view the book' do
     click_link('View', match: :first)
@@ -24,9 +24,9 @@ RSpec.describe 'Admin Books' do
 
   it 'when admin delete the book' do
     expect(Book.count).to eq(1)
-    admin_books_page.accept_confirm{ click_link('Delete', match: :first, visible: true)}
+    admin_books_page.accept_confirm { click_link('Delete', match: :first, visible: true) }
     expect(admin_books_page).to have_current_path(admin_books_path)
-    expect(admin_books_page).to have_content(I18n.t('admin.test.destroy',item: "Book"))
+    expect(admin_books_page).to have_content(I18n.t('admin.test.destroy', item: 'Book'))
     expect(Book.count).to eq(0)
   end
 end
