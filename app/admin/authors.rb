b7753a42f6c9e76ef399cfb74d6a1d1d565e6311
AnastasiusW <repositoryanastasiia@gmail.com>
2020-04-08 13:26:06 +0300

    After fix rubocop1

--- app/admin/authors.rb
+++ app/admin/authors.rb
@@ -1,11 +1,10 @@
 ActiveAdmin.register Author do
-  #actions :all, except: :destroy
-   permit_params :first_name, :last_name, :biography
-   remove_filter :authors_books
-   #config.remove_action_item(:destroy)
+  # actions :all, except: :destroy
+  permit_params :first_name, :last_name, :biography
+  remove_filter :authors_books
+  # config.remove_action_item(:destroy)
 
-
-   index do
+  index do
     selectable_column
     id_column
     column :first_name
@@ -20,6 +19,6 @@ ActiveAdmin.register Author do
     column I18n.t('active_admin.delete') do |author|
       link_to I18n.t('active_admin.delete'), admin_author_path(author),
               method: :delete, data: { confirm: t('admin.authors.confirmations', quantity: author.books.count) }
-   end
+    end
   end
 end
