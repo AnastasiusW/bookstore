89d35782902755d0abb966150008b90e50c645ad
AnastasiusW <repositoryanastasiia@gmail.com>
2020-05-04 20:20:36 +0300

    Add correct to carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.config[:aws][:key],
       aws_secret_access_key: Rails.application.credentials.config[:aws][:secret_key],
-      region: Rails.application.credentials.config[:aws][:region]
+      region: Rails.application.credentials.config[:aws][:region],
     }
     config.storage = :fog
     config.fog_directory = Rails.application.credentials.config[:aws][:bucket]
