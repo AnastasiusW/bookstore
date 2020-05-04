b686733a7302d09bdee7a685c68f4a61b3a0a26d
AnastasiusW <repositoryanastasiia@gmail.com>
2020-05-04 23:21:23 +0300

    Add region to config carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.config[:aws][:key],
-      aws_secret_access_key: Rails.application.credentials.config[:aws][:secret_key]
+      aws_secret_access_key: Rails.application.credentials.config[:aws][:secret_key],
+      region: Rails.application.credentials.config[:aws][:region]
     }
     config.storage = :fog
     config.fog_directory = Rails.application.credentials.config[:aws][:bucket]
