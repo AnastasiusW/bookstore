02dd1d4ad2d442a82db275f7301a99e4cb7fbec1
AnastasiusW <repositoryanastasiia@gmail.com>
2020-05-04 20:24:38 +0300

    Remove regin from config carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.config[:aws][:key],
-      aws_secret_access_key: Rails.application.credentials.config[:aws][:secret_key],
-      region: Rails.application.credentials.config[:aws][:region],
+      aws_secret_access_key: Rails.application.credentials.config[:aws][:secret_key]
     }
     config.storage = :fog
     config.fog_directory = Rails.application.credentials.config[:aws][:bucket]
