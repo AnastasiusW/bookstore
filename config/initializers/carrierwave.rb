3aa9de3889dd509b2f6f0ec1bd6471befd8a2367
AnastasiusW <repositoryanastasiia@gmail.com>
2020-05-05 14:29:36 +0300

    Little fix carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,6 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.config[:aws][:access_key_id],
