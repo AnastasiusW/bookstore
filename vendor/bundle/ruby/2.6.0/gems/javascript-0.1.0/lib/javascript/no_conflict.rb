399a973b61a704af9675a15b14b77bbe630ec09c
AnastasiusW <repositoryanastasiia@gmail.com>
2020-05-07 14:47:24 +0300

    Remove vendor/bundle and set chown user

--- vendor/bundle/ruby/2.6.0/gems/javascript-0.1.0/lib/javascript/no_conflict.rb
+++
@@ -1,5 +0,0 @@
-module JavaScript
-  NO_CONFLICT = true
-end
-
-require "javascript"
--- vendor/bundle/ruby/2.6.0/gems/site_prism-all_there-0.3.2/lib/site_prism/all_there/version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module SitePrism
-  module AllThere
-    # @return [String]
-    # Version of the gem
-    VERSION = '0.3.2'
-  end
-end
