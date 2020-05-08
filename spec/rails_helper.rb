47f5acf7cfb1c24fb8499c512d56b2e06cd4cd63
AnastasiusW <repositoryanastasiia@gmail.com>
2020-05-08 22:27:09 +0300

    Fix rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.after(:all) do
-    FileUtils.rm_rf(Dir["#{Rails.root}/public/test"]) if Rails.env.test?
+    FileUtils.rm_rf(Dir["#{Rails.root}/public/test"])
   end
 end
