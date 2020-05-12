30d9954a9c69f040317d2dd6cc4cc120fdf75594
AnastasiusW <repositoryanastasiia@gmail.com>
2020-05-12 19:49:19 +0300

    Add model line_item and coupon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,14 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  helper_method :current_order
 
   def header_display
     @presenter_header = Presenters::Header.new
+  end
+
+  def current_order
+    Order.find_by(id: session[:order_id]) || Order.create
   end
 
   private
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
   attr_accessor :active_admin_requested_event
 
   belongs_to :user, optional: true
+  has_many :line_items, dependent: destroy
 
   enum status: {
     in_progress: 1,
