97d36126e2d576bf95e38dbf01ca1af5ea720aa7
AnastasiusW <repositoryanastasiia@gmail.com>
2020-06-06 17:20:26 +0300

    Add transaction to step address and js

--- app/lib/services/checkout/update/address.rb
+++ app/lib/services/checkout/update/address.rb
@@ -6,11 +6,13 @@ module Services
           @current_order = order
           @order_billing_address = billing
           @order_shipping_address = shipping
-          @use_billing_address = use_billing
+          @use_billing_address = use_billing.to_i
         end
 
         def call
+
           if @use_billing_address == 1
+
             @order_shipping_address = @order_billing_address.clone
             @order_shipping_address[:type] = 'shipping_address'
           end
@@ -18,17 +20,21 @@ module Services
         end
 
         def save_addresses
+
           ActiveRecord::Base.transaction do
-            @form_billing = AddressForm.new(params: @order_billing_address, current_instance: @current_order)
-            @form_shipping = AddressForm.new(params: @order_shipping_address, current_instance: @current_order)
-            @form_billing.save
-            @form_shipping.save
+            @form_billing = AddressForm.new(params: @order_billing_address, current_instance: @current_order).save
+            @form_shipping = AddressForm.new(params: @order_shipping_address, current_instance: @current_order).save
+
             update_use_billing_address
+            if  !@form_billing || !@form_shipping
+              raise ActiveRecord::Rollback
+            end
+
           end
         end
 
         def update_use_billing_address
-          @current_order.update(use_billing_address: @use_billing_address)
+          @current_order.update!(use_billing_address: @use_billing_address)
         end
       end
     end
