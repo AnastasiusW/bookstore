284879140b6f9320fbbab12eae7cf08e99e477ec
AnastasiusW <repositoryanastasiia@gmail.com>
2020-06-07 17:51:43 +0300

    Fisnish delivery substep

--- app/lib/presenters/line_items/index.rb
+++ app/lib/presenters/line_items/index.rb
@@ -35,6 +35,14 @@ module Presenters
       def delivery_methods
         Delivery.all
       end
+
+      def checked_delivery(delivery_id)
+        order_delivery_exists?(delivery_id) || Delivery.first.id==delivery_id
+      end
+
+      def order_delivery_exists?(delivery_id)
+        @current_order.delivery_id && @current_order.delivery_id== delivery_id
+      end
     end
   end
 end
---
+++ app/lib/services/checkout/update/delivery.rb
@@ -0,0 +1,31 @@
+module Services
+  module Checkout
+    module Update
+      class Delivery
+        def initialize(order:, delivery_params:)
+          @current_order = order
+          @delivery_params = delivery_params[:delivery_id].to_i
+
+        end
+
+        def call
+          save_delivery_method
+        end
+
+        def save_delivery_method
+          ActiveRecord::Base.transaction do
+            begin
+           @current_order.update!(delivery_id: @delivery_params )
+           Services::Orders::RecalculateAmount.new(@current_order).call
+          rescue ActiveRecord::InvalidForeignKey
+            raise ActiveRecord::Rollback
+           end
+          end
+
+        end
+
+
+      end
+    end
+  end
+end
