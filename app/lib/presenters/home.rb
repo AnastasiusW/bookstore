03d5a08be15ab05ea04662e22c14c0048453749c
AnastasiusW <repositoryanastasiia@gmail.com>
2020-06-20 20:51:55 +0300

    Add controller and service sort tests

--- app/lib/presenters/home.rb
+++ app/lib/presenters/home.rb
@@ -2,8 +2,8 @@ module Presenters
   class Home
     def initialize(best_sellers:)
       @best_sellers = best_sellers
-
     end
+
     def latest_books
       BookDecorator.decorate_collection(Book.latest)
     end
